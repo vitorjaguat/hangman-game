71682b622a0c8a324fd09df53a8737f5e0baee89
Vitor Jaguat <vitorbutkus@gmail.com>
2022-10-25 16:53:57 -0300

    update words

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const finalMessage = document.getElementById('final-message');
 
 const figureParts = document.querySelectorAll('.figure-part');
 
-const words = ['application', 'programming', 'interface', 'wizard'];
+const words = ['antilope', 'jaguat', 'cerejinha', 'bolsinha', 'michui', 'picanha', 'paleta', 'jaguatilope'];
 
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 
